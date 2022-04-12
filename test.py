05dc38e6464bf70a1b228d1ea24756c775e98410
DESKTOP-DQGOI1A\dohyu <dohyun201@gmail.com>
2022-04-12 19:38:21 +0900

    시간변경

--- test.py
+++ test.py
@@ -58,7 +58,7 @@ async def findCoin():
         #log.debug("  ")
 
         #오전 8시 ~ 9시에 전량매도 
-        if(int(endTime) < 900 and int(endTime) > 841):
+        if(int(endTime) < 900 and int(endTime) > 831):
             CoinEvent.allSelCoin()
             await asyncio.sleep(10)   
 
@@ -72,7 +72,7 @@ async def findCoin():
                     CoinEvent.getMyPaymentList()  
                 except Exception as Err:
                     log.debug('[[[[[[Error]]]]]] getMyPaymentList Error>>>'+str(Err))
-        elif(int(endTime) < 840 and int(endTime) > 800):
+        elif(int(endTime) < 830 and int(endTime) > 800):
             #log.debug('휴식시간---------------- 8시 ~~~ 8시 53분')
             print("휴식시간")
         else :
